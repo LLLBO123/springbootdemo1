c5019486e9fe28a8fe31e3bfac0b0f71e711601d
liaohaha <645741292@qq.com>
2023-08-08 23:37:46 +0800

    test1_20230808_lyp

--- src/main/java/com/example/springbootdemo1/controller/HelloCotroller.java
+++ src/main/java/com/example/springbootdemo1/controller/HelloCotroller.java
@@ -2,6 +2,7 @@ package com.example.springbootdemo1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,11 @@ public class HelloCotroller {
 //    @RequestMapping()
     public String hello(){
         return "hello world";
+    }
+
+    @RequestMapping(value = "/hello1",method = RequestMethod.POST)
+    public String hello1(){
+        System.out.println("nihao");
+        return "ni hao /post";
     }
 }
